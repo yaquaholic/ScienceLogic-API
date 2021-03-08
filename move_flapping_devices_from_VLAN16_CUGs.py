ae20da40d9fd7e5dbe1cecc101f387b828f30dab
Yaquaholic <richard@aqueouslife.co.uk>
2021-03-08 17:19:49 +0000

    Add files via upload
    First pulls the device flapping events from the API, then sorts them for unique values, then gets the unique devices details.
    If they exist in any of the VLAN16  collector groups, re-assign the CUG to be the new test CUG. 
    The actual post_api() call is commented out, for testing purposes.

---
+++ move_flapping_devices_from_VLAN16_CUGs.py
@@ -0,0 +1,56 @@
+## Find flapping devices, from CSC CUG
+## and re-assign to new collector group
+
+#!/usr/bin/env python
+
+from sciencelogic_api_functions import get_api, post_api
+
+device_list = []
+cug_list = ['/api/collector_group/3', '/api/collector_group/8']
+
+#Flapping cleared events (policy 4011)
+limit = 100
+print("Collecting the last " + str(limit) + " cleared flapping events. \n")
+url = '/api/cleared_event?limit=' + str(limit) + '&order.date_last=desc&filter.0.event_policy.eq=%2Fapi%2Fevent_policy%2F4011'
+events = get_api(url)
+
+for event in events:
+    event_url = event['URI']
+    event_details = get_api(event_url)
+    
+    device_url = event_details['aligned_resource']
+    device_list.append(device_url)
+
+#Make list unique values only
+print("Events collected, making list unique for next step \n")
+device_list = set(device_list)
+
+for device_url in device_list:
+    device_details = get_api(device_url)
+    did = device_url.replace('/api/device/','')
+
+    name = device_details['name']
+    cug = device_details['collector_group']
+    
+
+
+    if cug in cug_list:
+        print("\n" + name + " " + did + " is currently in " + cug )
+        data = {'collector_group': '/api/collector_group/14'}
+        print("Updating CUG for " + name + "\n")
+        
+        ## Update CUG to 14
+        #post_api(device_url,data)
+        
+    else:
+        print( name + " is in " + cug + " so ignore it" )
+        
+        
+
+    
+
+    
+    
+
+    
+
